61942f6daf8a1337cab33db535733726ce2eeff0
Swayam Awate <swayam@awate.local>
2026-04-09 01:06:36 +0530

    added while loop for calculations

--- Calculator.java
+++ Calculator.java
@@ -7,46 +7,55 @@ public class Calculator {
         Scanner reader = new Scanner(System.in);
 
         System.out.println("--- Java Console Calculator ---");
-        
-        // Input first number
-        System.out.print("Enter first number: ");
-        double first = reader.nextDouble();
 
-        // Input operator
-        System.out.print("Enter an operator (+, -, *, /): ");
-        char operator = reader.next().charAt(0);
-
-        // Input second number
-        System.out.print("Enter second number: ");
-        double second = reader.nextDouble();
-
-        double result;
-
-        // Mathematical Logic
-        switch (operator) {
-            case '+':
-                result = first + second;
+        while (true) {
+             // Input first number
+            if (false) {
                 break;
-            case '-':
-                result = first - second;
-                break;
-            case '*':
-                result = first * second;
-                break;
-            case '/':
-                if (second != 0) {
-                    result = first / second;
-                } else {
-                    System.out.println("Error! Division by zero is not allowed.");
+            }
+            System.out.println("Press Ctrl+C to exit.");
+            System.out.print("Enter first number: ");
+            double first = reader.nextDouble();
+
+            // Input operator
+            System.out.print("Enter an operator (+, -, *, /): ");
+            char operator = reader.next().charAt(0);
+
+            // Input second number
+            System.out.print("Enter second number: ");
+            double second = reader.nextDouble();
+
+            double result;
+
+            // Mathematical Logic
+            switch (operator) {
+                case '+':
+                    result = first + second;
+                    break;
+                case '-':
+                    result = first - second;
+                    break;
+                case '*':
+                    result = first * second;
+                    break;
+                case '/':
+                    if (second != 0) {
+                        result = first / second;
+                    } else {
+                        System.out.println("Error! Division by zero is not allowed.");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("Error! Operator is not correct.");
                     return;
-                }
-                break;
-            default:
-                System.out.println("Error! Operator is not correct.");
-                return;
-        }
 
-        System.out.printf("%.2f %c %.2f = %.2f%n", first, operator, second, result);
+            }
+            System.out.printf("%.2f %c %.2f = %.2f%n", first, operator, second, result);
+
+        }
+        
+        
         reader.close();
     }
 }
